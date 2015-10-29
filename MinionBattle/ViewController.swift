aa75075b1a05a19e4d902f88bebea09f804cd09f
Pham Nguyen Hai An <phamnguyenhaian@k2digital.local>
2015-10-29 14:47:25 +0700

    Initial Commit

---
+++ MinionBattle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MinionBattle
+//
+//  Created by Pham Nguyen Hai An on 10/29/15.
+//  Copyright © 2015 k2digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
