bd4089c788da8f2c09e0651985845e48c3354ed2
Pham Nguyen Hai An <phamnguyenhaian@k2digital.local>
2015-11-09 05:43:07 +0700

    Minion Description Project

--- MinionBattle/ViewController.swift
+++ MinionBattle/ViewController.swift
@@ -10,6 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var crazy: UIImageView!
+    
+    @IBOutlet weak var normal: UIImageView!
+    
+    @IBOutlet weak var btnCrazy: UIButton!
+    
+    @IBOutlet weak var btnNormal: UIButton!
+    
+    @IBAction func makeMeCrazy(sender: AnyObject) {
+        
+        normal.hidden = true
+        crazy.hidden = false
+        btnCrazy.hidden = true
+        btnNormal.hidden = false
+        
+    }
+    
+    
+    @IBAction func makeMeNormal(sender: AnyObject) {
+        
+        normal.hidden = false
+        crazy.hidden = true
+        btnCrazy.hidden = false
+        btnNormal.hidden = true
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
